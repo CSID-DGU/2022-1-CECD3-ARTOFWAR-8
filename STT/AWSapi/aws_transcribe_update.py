5a772e7167aa6d3b4ec4ff3b3e5c7f1d8e32a396
JinHabin <80958412+JinHabin@users.noreply.github.com>
2022-12-19 15:07:42 +0900

    Update aws_transcribe_update.py

--- STT/AWSapi/aws_transcribe_update.py
+++ STT/AWSapi/aws_transcribe_update.py
@@ -4,13 +4,13 @@ import boto3
 from botocore.config import Config
 from analysis_text import analysisVIDText
 
-bucket = 'cecd3'
+bucket = 'cecd3'   # cecd3 버킷에서 실행
 
 def transcribe(vid_title):
     file_nm = vid_title
 
     # Transcribe를 위한 Config 설정
-    my_config = Config(
+    vid_config = Config(
     	region_name = 'us-east-1',
         signature_version = 'v4',
         retries={
@@ -20,9 +20,9 @@ def transcribe(vid_title):
         )
 
     # Transcribe 실행
-    transcribe =boto3.client('transcribe', config=my_config)
-    # s3에 업로드한 파일 URL
-    job_uri = 'https://{}.s3.amazonaws.com/{}.mp4'.format(bucket,file_nm)
+    transcribe =boto3.client('transcribe', config=vid_config)
+    # s3에 저장된 동영상 파일 로드
+    job_uri = 'https://{}.s3.amazonaws.com/{}.mp4'.format(bucket,file_nm)   # 생성된 job의 링크 형식에 맞춤
     transcribe.start_transcription_job(
         TranscriptionJobName=file_nm,
         Media={'MediaFileUri': job_uri},
@@ -38,31 +38,30 @@ def transcribe(vid_title):
     while True:
         status = transcribe.get_transcription_job(TranscriptionJobName=file_nm)
         if status['TranscriptionJob']['TranscriptionJobStatus'] in ['COMPLETED', 'FAILED']:
-            save_json_uri = status['TranscriptionJob']['Transcript']['TranscriptFileUri']
+            json_uri = status['TranscriptionJob']['Transcript']['TranscriptFileUri']
             break
 
-    # Transcribe 결과가 저장된 웹주소
-    save_json_uri = status['TranscriptionJob']['Transcript']['TranscriptFileUri']
+    json_uri = status['TranscriptionJob']['Transcript']['TranscriptFileUri']
 
-     # 웹서버 결과 파이썬으로 불러오기
+     # 웹서버 결과
     load = request.urlopen(save_json_uri)
     confirm = load.status
     rst = load.read().decode('utf-8')
 
-    # 문자열을 딕셔너리로 변환 후 결과 가져오기
-    transcribe_text = literal_eval(rst)['results']['transcripts'][0]['transcript']
+    # 문자열을 딕셔너리
+    transcribe_res = literal_eval(rst)['results']['transcripts'][0]['transcript']
 
     # 확인
-    print(transcribe_text)
+    print(transcribe_res)
 
     # Job 삭제하기
-    del_transcribe =boto3.client('transcribe', config=my_config)
-    res = del_transcribe.delete_transcription_job(
+    del_transcribe =boto3.client('transcribe', config=vid_config)
+    restext = del_transcribe.delete_transcription_job(
     TranscriptionJobName = file_nm
     )
 
-    res['ResponseMetadata']['HTTPStatusCode'] == '200'
-    return analysisVIDText(transcribe_text)
+    restext['ResponseMetadata']['HTTPStatusCode'] == '200'
+    return analysisVIDText(transcribe_res)
 
 
 #임시로 직접 제목 입력
